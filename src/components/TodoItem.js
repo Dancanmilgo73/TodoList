a210406901d8d6973630fa704288864bc71fa31e
Dancanmilgo73 <dancanmilgo73@gmail.com>
2022-01-18 11:10:12 +0300

    fixing responsiveness and configuring project for deployment

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,18 +3,19 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, isComplete } from "../redux/actions/todoActions";
 
 export default function TodoItem({ title, id, completed }) {
-  const dispatch = useDispatch();
-  // const [complete, setComplete] =useState(false);
+	const dispatch = useDispatch();
+	// const [complete, setComplete] =useState(false);
 
-  return (
-    <div className={`todoItem__con ${completed && "todoComplete"}`}>
-      <div>
-        <input type="checkbox" onChange={() => dispatch(isComplete(id))} />
-        <span>{title}</span>
-      </div>
-      <div className="todoItem__remove">
-        <button onClick={() => dispatch(deleteTodo(id))}>Delete</button>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`todoItem__con ${completed && "todoComplete"}`}>
+			<div>
+				<input type='checkbox' onChange={() => dispatch(isComplete(id))} />
+			</div>
+			<p>{title}</p>
+
+			<div className='todoItem__remove'>
+				<button onClick={() => dispatch(deleteTodo(id))}>Delete</button>
+			</div>
+		</div>
+	);
 }
--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,26 +1,31 @@
 import { ADDTODO, REMOVE__TODO, TODO__ISCOMPLETE } from "../actionTypes";
-export const todoReducer = (state = { todos: [] }, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    case ADDTODO:
-      return { ...state, todos: [...state.todos, payload] };
-    case REMOVE__TODO:
-      return {
-        todos: [...state.todos.filter((todo) => todo.id !== payload)],
-      };
-    case TODO__ISCOMPLETE:
-      return {
-        ...state,
-        todos: [
-          ...state.todos.map((todo) => {
-            if (todo.id === payload) {
-              todo.completed = !todo.completed;
-            }
-            return todo;
-          }),
-        ],
-      };
-    default:
-      return { ...state };
-  }
+export const todoReducer = (
+	state = {
+		todos: [{ title: "Remember to add to do", id: 1, completed: false }],
+	},
+	action
+) => {
+	const { type, payload } = action;
+	switch (type) {
+		case ADDTODO:
+			return { ...state, todos: [...state.todos, payload] };
+		case REMOVE__TODO:
+			return {
+				todos: [...state.todos.filter((todo) => todo.id !== payload)],
+			};
+		case TODO__ISCOMPLETE:
+			return {
+				...state,
+				todos: [
+					...state.todos.map((todo) => {
+						if (todo.id === payload) {
+							todo.completed = !todo.completed;
+						}
+						return todo;
+					}),
+				],
+			};
+		default:
+			return { ...state };
+	}
 };
